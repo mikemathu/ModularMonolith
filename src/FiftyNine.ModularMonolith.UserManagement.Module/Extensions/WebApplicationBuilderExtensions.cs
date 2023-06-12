ec5eb728f650966d292e60c9119526625871475e
Michael Mathu <mikemathu.dev@gmail.com>
2023-06-12 12:55:37 +0300

    Add project files.

---
+++ src/FiftyNine.ModularMonolith.UserManagement.Module/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FiftyNine.ModularMonolith.UserManagement.Module.Data;
+
+namespace FiftyNine.ModularMonolith.UserManagement.Module.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddUserManagement(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers()
+                        .AddApplicationPart(typeof(WebApplicationBuilderExtensions).Assembly);
+
+        builder.Services.AddSingleton<Users>()
+                        .AddSingleton<Data.IUsers>(x => x.GetRequiredService<Users>())
+                        .AddSingleton<IUsers>(x => x.GetRequiredService<Users>());
+
+        return builder;
+    }
+}
